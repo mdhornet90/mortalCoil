60660f270e5a4e4316ee8695bf339c2f309da4b7
Chris <cmurph14@villanova.edu>
2011-01-08 00:21:34 -0500

    Added comments and the initialization of the solver in main

--- mortal.c
+++ mortal.c
@@ -7,58 +7,71 @@ struct path
     int horizX;
     int vertY;
     int direction[4];
-    int startPos;
-    int curPos; //Current position
+    int lastDir;
+    int spacesLeft;
     char *board;
 };
 
 int main(void)
 {
     //Prototypes
-    void addBorder(struct path* arg);
+    void initBoard(struct path* arg);
     void printBoard(struct path *arg);
+    void solver(struct path *arg);
     //Variables
     struct path level;
-    int area;
+    int area, i = 0;
 
     printf("Receiving board...\n");
-    scanf("%i %i", &level.horizX, &level.vertY);
+    scanf("%i %i", &level.horizX, &level.vertY); //Takes its inputs from the command curl -s http://www.hacker.org/coil/ |
+                                                 //grep -m 1 "FlashVars" | grep -o "x[^\"]*"| sed "s/[^0-9.X]\+/ /g"|./mortal
     area = level.horizX * level.vertY;
 
-    level.board = (char*)malloc(area * sizeof(char));
+    level.board = (char*)malloc(area * sizeof(char)); //Allocate enough spaces for initial board (to be overwritten later)
     scanf("%s", level.board);
 
-    level.direction[0] = 1;
+    level.direction[0] = 1; //Defining directions
     level.direction[1] = level.horizX;
     level.direction[2] = -1;
     level.direction[3] = -level.horizX;
 
-    printf("Before adding border:\n\n");
-    printBoard(&level);
+    initBoard(&level);
 
-    addBorder(&level);
+    while( level.spacesLeft != 0 ) //Will exit loop when path has been completely traversed
+    {
+        while(level.board[i] == 'X') //Finds its first starting position the first time through
+            i++;
 
-    printf("After adding border:\n\n");
-    printBoard(&level);
+        level.lastDir = 0;
+        solver(&level);
+        if( level.spacesLeft == 0 )
+            break;
+        level.lastDir++;
+        solver(&level);
+    }
 
     return 0;
 }
 
+void solver(struct path *arg)
+{
+}
 
-
-void addBorder(struct path* arg)
+void initBoard(struct path* arg)
 {
     struct path *tmp = arg;
+    tmp->spacesLeft = 0;
 
-    tmp->horizX += 2;
+    tmp->horizX += 2;                                   //Set new board dimensions
     tmp->vertY += 2;
     int newArea = tmp->horizX * tmp->vertY;
-    char *tempBoard = (char *)malloc(sizeof(char) * newArea);
+    char *tempBoard = (char *)malloc(sizeof(char) * newArea); //temporary board
     int i, j, k = 0, l;
 
-    for ( i = 0; i < (tmp->horizX); i++ )
+    for ( i = 0; i < (tmp->horizX); i++ ) //All X's as the top row
         tempBoard[i] = 'X';
-    for( j = 0; j < tmp->vertY - 2; i++, j++ )
+
+    for( j = 0; j < tmp->vertY - 2; i++, j++ ) //Interweave original board with more X's as border
     {
         tempBoard[i] = 'X';
         i++;
@@ -66,12 +79,17 @@ void addBorder(struct path* arg)
             tempBoard[i] = arg->board[k];
         tempBoard[i] = 'X';
     }
-    for ( i; i < newArea; i++ )
+
+    for ( i; i < newArea; i++ ) //All X's as the bottom row
         tempBoard[i] = 'X';
 
-    free(tmp->board);
-    tmp->board = (char *)malloc(sizeof(char) * newArea);
-    tmp->board = tempBoard;
+    free(tmp->board); //Prevent memory leaks
+    tmp->board = (char *)malloc(sizeof(char) * newArea); //Resize board
+    tmp->board = tempBoard; //Move new board in
+
+    for ( i = 0; i< newArea; i++ ) //Count the initial number of free spaces the program will have to traverse
+        if( tmp->board[i] == '.' )
+            tmp->spacesLeft++;
 
     return;
 }
@@ -81,7 +99,7 @@ void printBoard(struct path *arg)
     int i = 0, j;
     struct path *tmp = arg;
 
-    for( j = 0; j < tmp->vertY; j++ )
+    for( j = 0; j < tmp->vertY; j++ ) //Parsing the board for printing so it appears as a neat 2D array
     {
         printf("    ");
         for( i; i < tmp->horizX * (j + 1); i++ )
