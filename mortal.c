8cac3b7df06e0707cd5c7d170f24dbb7a657ea43
Chris <cmurph14@villanova.edu>
2011-01-07 22:59:50 -0500

    Ch-ch-changes

--- mortal.c
+++ mortal.c
@@ -1,87 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdbool.h>
 
 struct path
 {
-    struct path *prev;
-    bool leftRight[2];
-    bool upDown[2];
-    int position;
-    int spacesMoved;
-    struct path *next;
-};
-
-struct level
-{
+    int horizX;
+    int vertY;
+    int direction[4];
+    int startPos;
+    int curPos; //Current position
     char *board;
-    int numberSolved;
-    int curPosition;
-    int size;
-    int totalSolved;
-    int x;
-    int y;
-    struct path traversal;
 };
 
 int main(void)
 {
-    int i, X = 0;
-    void solver(struct level *arg);
-    struct level progLevel;
-    char temp[100];
+    //Prototypes
+    void addBorder(struct path* arg);
+    void printBoard(struct path *arg);
+    //Variables
+    struct path level;
+    int area;
 
-    scanf("%i %i %s", &progLevel.x, &progLevel.y, temp);
+    printf("Receiving board...\n");
+    scanf("%i %i", &level.horizX, &level.vertY);
+    area = level.horizX * level.vertY;
 
-    progLevel.board = temp;
-    progLevel.curPosition = 0;
-    progLevel.totalSolved = 0;
-    progLevel.traversal.prev = 0;
-    progLevel.traversal.next = 0;
-    progLevel.traversal.upDown[0] = 0;
-    progLevel.traversal.leftRight[0] = 0;
+    level.board = (char*)malloc(area * sizeof(char));
+    scanf("%s", level.board);
 
-    for ( i = 0; i < (progLevel.x * progLevel.y); i++ )
-        if ( progLevel.board[i] == 'X' )
-            X++;
+    level.direction[0] = 1;
+    level.direction[1] = level.horizX;
+    level.direction[2] = -1;
+    level.direction[3] = -level.horizX;
 
-    progLevel.size = (progLevel.x * progLevel.y) - X;
+    printf("Before adding border:\n\n");
+    printBoard(&level);
+
+    addBorder(&level);
+
+    printf("After adding border:\n\n");
+    printBoard(&level);
 
     return 0;
 }
 
-void solver(struct level *arg)
-{
-    bool testUp(struct level *arg);
-    void newPath(struct path *arg);
-    struct level *tmp = arg;
-    while(1)
-    {
-        while( tmp->board[tmp->curPosition] == 'X' )
-            tmp->curPosition++;
-        if( testUp(tmp) )
-        {
-            newPath(&(tmp->traversal));
-            while( testUp(tmp) )
-            {
-            }
-        }
-    }
-}
 
-bool testUp(struct level *arg)
-{
-    struct level *tmp = arg;
-    return true;
-}
 
-void newPath(struct path *arg)
+void addBorder(struct path* arg)
 {
     struct path *tmp = arg;
-    struct path *newEntry = (struct path *) malloc(sizeof(struct path));
 
-    tmp->next = newEntry;
-    newEntry->prev = tmp;
-    newEntry->next = 0;
+    tmp->horizX += 2;
+    tmp->vertY += 2;
+    int newArea = tmp->horizX * tmp->vertY;
+    char *tempBoard = (char *)malloc(sizeof(char) * newArea);
+    int i, j, k = 0, l;
+
+    for ( i = 0; i < (tmp->horizX); i++ )
+        tempBoard[i] = 'X';
+    for( j = 0; j < tmp->vertY - 2; i++, j++ )
+    {
+        tempBoard[i] = 'X';
+        i++;
+        for( l = 0; l < tmp->horizX - 2; i++, k++, l++ )
+            tempBoard[i] = arg->board[k];
+        tempBoard[i] = 'X';
+    }
+    for ( i; i < newArea; i++ )
+        tempBoard[i] = 'X';
+
+    free(tmp->board);
+    tmp->board = (char *)malloc(sizeof(char) * newArea);
+    tmp->board = tempBoard;
+
+    return;
+}
+
+void printBoard(struct path *arg)
+{
+    int i = 0, j;
+    struct path *tmp = arg;
+
+    for( j = 0; j < tmp->vertY; j++ )
+    {
+        printf("    ");
+        for( i; i < tmp->horizX * (j + 1); i++ )
+            printf("%c ", tmp->board[i]);
+        printf("\n");
+    }
+    printf("\n");
+
+    return;
 }
